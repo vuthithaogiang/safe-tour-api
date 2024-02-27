48e016e6ade8b70843990caee9e5555c71176fd1
giang <vuthithaogiang050400@gmail.com>
2024-02-27 17:30:13 +0700

    add doFilterInternal check authHeader token Bearer each request and bypass public request

--- shopapp/src/main/java/com/project/shopapp/components/JwtTokenUtil.java
+++ shopapp/src/main/java/com/project/shopapp/components/JwtTokenUtil.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.InvalidParameterException;
@@ -93,5 +94,13 @@ public class JwtTokenUtil {
         return expirationDate.before(new Date());
     }
 
+    public String extractPhoneNumber(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
+    public  boolean validateToken(String token, UserDetails userDetails) {
+        String phoneNumber = extractPhoneNumber(token);
+
+        return (phoneNumber.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 }
