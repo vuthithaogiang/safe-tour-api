b5d093a352c76c6e8c0cd1c0972bdee11874a587
giang <vuthithaogiang050400@gmail.com>
2024-02-27 15:45:43 +0700

    setup Maven repository, config securities, register user and return jwt token sub sequense login

--- shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
+++ shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
+import com.project.shopapp.models.User;
 import com.project.shopapp.services.IUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -39,8 +40,9 @@ public class UserController {
             if(!userDTO.getPassword().equals(userDTO.getRetypePassword())) {
                 return ResponseEntity.badRequest().body("Password does not match");
             }
-            userService.register(userDTO);
-            return ResponseEntity.ok("Register successfully");
+            User user = userService.register(userDTO);
+           // return ResponseEntity.ok("Register successfully");
+            return ResponseEntity.ok(user);
         }
         catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
@@ -50,9 +52,15 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        try{
+           String token = userService.login(userLoginDTO);
+            return ResponseEntity.ok(token);
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
-        String token = userService.login(userLoginDTO);
-        return ResponseEntity.ok("Some tokens");
+
     }
 
     @GetMapping()
--- shopapp/src/main/java/com/project/shopapp/services/UserService.java
+++ shopapp/src/main/java/com/project/shopapp/services/UserService.java
@@ -1,6 +1,7 @@
 package com.project.shopapp.services;
 
 
+import com.project.shopapp.components.JwtTokenUtil;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.dtos.UserLoginDTO;
 import com.project.shopapp.exceptions.DataNotFoundException;
@@ -10,9 +11,14 @@ import com.project.shopapp.repositories.RoleRepository;
 import com.project.shopapp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +27,13 @@ public class UserService implements IUserService{
     private final UserRepository userRepository;
 
     private final RoleRepository roleRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    private  final AuthenticationManager authenticationManager;
+
     @Override
     public User register(UserDTO userDTO) throws DataNotFoundException {
         String phoneNumber = userDTO.getPhoneNumber();
@@ -28,7 +41,6 @@ public class UserService implements IUserService{
         if(userRepository.existsByPhoneNumber(phoneNumber)) {
             throw new DataIntegrityViolationException("Phone number has existed.");
         }
-
 
         User newUser = User
                    .builder()
@@ -47,15 +59,46 @@ public class UserService implements IUserService{
 
         if(userDTO.getFacebookAccountId() == 0 && userDTO.getGoogleAccountId() == 0) {
             String password = userDTO.getPassword();
-          //  String encodePassword = passwordEncoder.encode(password);
-          //  newUser.setPassword(encodePassword);
+            String encodePassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodePassword);
         }
         return userRepository.save(newUser);
     }
 
     @Override
-    public String login(UserLoginDTO userLoginDTO) {
-        return null;
+    public String login(UserLoginDTO userLoginDTO) throws Exception{
+        Optional<User> optionalUser = userRepository.findByPhoneNumber(userLoginDTO.getPhoneNumber());
+
+        if(optionalUser.isEmpty()) {
+            throw new
+                    DataNotFoundException("Not found user, " +
+                    "invalid phone number or password");
+        }
+
+        User existingUser = optionalUser.get();
+
+        //check password
+        if(existingUser.getFacebookAccountId() == 0 &&
+        existingUser.getGoogleAccountId() == 0) {
+            if(!passwordEncoder.matches(userLoginDTO.getPassword(),
+                    existingUser.getPassword())){
+                throw  new BadCredentialsException("Wrong phone number or password");
+            }
+        }
+
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        userLoginDTO.getPhoneNumber(),
+                        userLoginDTO.getPassword(),
+                        existingUser.getAuthorities()
+                );
+
+        authenticationManager.authenticate(authenticationToken);
+
+
+        //return jwt token
+         return jwtTokenUtil.generateToken(existingUser);
     }
 
     @Override
